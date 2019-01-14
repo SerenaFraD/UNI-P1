c8d848f5ea7d68166c58c4ce562064905a44cc0b
SerenaD <dursoserena61@gmail.com>
2019-01-14 21:28:05 +0100

    Add files via upload
    Permette di esaminare un file denominato albo.txt contentente l’elenco delle nazionali di calcio che hanno raggiunto le prime quattro
    posizioni in almeno un mondiale.
    Il file albo.txt é organizzato come segue. Ogni linea corrisponde ad una nazionale e presenta nell’ordine: il nome della nazionale, il numero di ori, il numero di argenti, il numero di bronzi e, infine, il numero di quarti posti. Ogni elemento é separato dagli altri da uno o più spazi. Il carattere ’-’ indica lo 0.
    
    Il programma pu`o essere usato in due modi:
    1) Passando il nome di una nazionale (es. mondiali Paesi Bassi). Esaminando il file, il programma stampa, su un’unica linea, la nazionale scelta, il numero di ori, argenti, bronzi, quarti posti e (fra parentesi) il numero totale di piazzamenti entro le prime tre posizioni;
    2) Passando la stringa max seguita da una fra le seguenti 4 possibili stringhe: oro, argento, bronzo, totale. Esaminando il file, il programma stampa la nazionale con il maggior numero di titoli (ori, argenti, bronzi o podi, a seconda della scelta), seguito dal numero di tali titoli.

---
+++ Mondiali/mondiali.c
@@ -0,0 +1,84 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#define NCHAR 100
+#define NNAME	30
+#define FILE_NAME "albo.txt"
+#define DIM 5
+
+void mondiali(char *parametri[], int nPar);
+
+int main(int argc, char *argv[]) {
+
+	if(argc < 2 && argc > 3) {
+		fprintf(stderr, "Passaggio errato dei parametri.\n");
+		printf("Uso: ./mon <'Nome nazione'>\n");
+		printf("Uso: ./mon <max> {<oro>, <argento>, <bronzo>, <totale>}\n");
+		exit(EXIT_FAILURE);
+	}
+	
+	mondiali(argv, argc);
+	
+	return 0;
+}
+
+void mondiali(char *parametri[], int nPar) {
+	char riga[NCHAR], nnation[NNAME], oro[DIM], argento[DIM], bronzo[DIM], totale[DIM], maxNat[NNAME];
+	FILE *fp;
+	int somma = 0, tmp, max = -1000, pos = 0;
+	short int isMax = 0, isNation = 0, isGold = 0, isSilver = 0, isBronze = 0, isTotal = 0;
+
+	if((fp = fopen(FILE_NAME, "r")) == NULL) {
+		fprintf(stderr, "Errore nell'apertura del file.\n");
+		exit(EXIT_FAILURE);
+	}
+	
+	if(strncmp(parametri[1], "max", 3) == 0) {
+		isMax = 1;
+		//printf("isMax\n");
+	
+		if(strcmp(parametri[2], "oro") == 0) {
+			isGold = 1;
+		} else if(strcmp(parametri[2], "argento") == 0) {
+			isSilver = 1;
+		} else if(strcmp(parametri[2], "bronzo") == 0) {
+			isBronze = 1;
+		} else if(strcmp(parametri[2], "totale") == 0) {
+			isTotal = 1;
+		}
+	}
+	
+	while(fgets(riga, sizeof(riga), fp)) {
+		sscanf(riga, "%[^0123456789-] %s %s %s %s", nnation, oro, argento, bronzo, totale);
+		//printf("%s %s %s %s %s\n", nnation, oro, argento, bronzo, totale);
+		
+		if(isMax) {
+			if(isGold){
+				tmp = atoi(oro);
+			} else if(isSilver) {
+				tmp = atoi(argento);
+			} else if(isBronze) {
+				tmp = atoi(bronzo);
+			} else if(isTotal) {
+				tmp = atoi(oro) + atoi(argento) + atoi(bronzo) ;
+			}
+			if(max < tmp) {
+				max = tmp;
+				strcpy(maxNat, nnation);
+			}
+		} else {
+			if(strncmp(parametri[1], nnation, strlen(parametri[1])) == 0) {
+				isNation = 1;
+				break;
+			}		
+		}
+	}
+	
+	if(isMax) {
+		printf("%s %d\n", maxNat, max);
+	} else if(isNation) {
+		printf("%s %s %s %s %s %d\n", nnation, ((strncmp(oro, "-", 2) == 0)? "0" : oro), argento, bronzo, totale, (atoi(oro) + atoi(argento) + atoi(bronzo)));
+	} else {
+		printf("Errore nell'immissione dei dati.\n");
+	}
+}
